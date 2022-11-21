7ef8ddc517b39651ca3a9233dff7aae90d63ada8
vagnerb79 <spotify.vagner@gmail.com>
2022-11-21 14:18:15 -0300

    Finalizado Módulo 1

--- modulo1/LogicaJavascript/index.js
+++ modulo1/LogicaJavascript/index.js
@@ -137,7 +137,7 @@ let trabalhoDeMatematica = 7.0
 let trabalhoDeCiencia = 10
 let trabalhoDeGeografia = 9.5
 
-let media (trabalhoDeHistoria + trabalhoDeMatematica + trabalhoDeCiencia + trabalhoDeGeografia) / 4
+let media = (trabalhoDeHistoria + trabalhoDeMatematica + trabalhoDeCiencia + trabalhoDeGeografia) / 4
 console.log(media)
 
 // ##########################################
