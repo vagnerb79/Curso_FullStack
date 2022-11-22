18bd40234f5846cae953946c9543bab1493af91b
vagnerb79 <spotify.vagner@gmail.com>
2022-11-22 11:48:32 -0300

    Iniciando Arrays

---
+++ modulo1/Arrays/index.js
@@ -0,0 +1,22 @@
+
+//Exercício 1
+// Podemos apenas criar arrays de strings?
+// E se eu quiser, por exemplo, representar os números da loteria que saíram na semana passada?
+// Ou as sucessivas jogadas de um dado?
+// Ou se der cara ou coroa em lançamentos sucessivos de uma moeda?
+
+// var numerosDaLoteria = [2, 11, 17, 32, 36, 39];
+// var jogadasDoDado= [1, 6, 6, 2, 2, 4];
+// var deuCara= [false, false, true, false];
+// var listaDeListasDeNumeros = [[1, 6], [6, 2, 2, 4]]
+// Como você pode ver, podemos ter arrays de números, strings, booleanos, etc. Podemos até ter arrays de arrays! 
+
+// Importante seguir sempre a mesma estrutura: organizamos os itens entre colchetes e separados por vírgulas. 
+
+// Vamos ver se ficou claro?
+
+// Crie dois arrays, seguindo as instruções abaixo:
+
+// - listaDePares: contendo todos os números pares de 1 a 10
+// - listaDeFilmes: contendo os títulos da trilogia do Senhor dos Anéis: A Sociedade do Anel, As Duas Torres e O Retorno do Rei.
+
