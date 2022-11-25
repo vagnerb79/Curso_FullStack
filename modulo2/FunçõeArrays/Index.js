a7262ca6cd382746180f8c5f8c6bd294ca4e6d9a
vagnerb79 <spotify.vagner@gmail.com>
2022-11-25 18:48:27 -0300

    Iniciando Módulo 2

---
+++ modulo2/FunçõeArrays/Index.js
@@ -0,0 +1,25 @@
+
+// Exercício I
+// A seguir, temos uma função expressa, que retorna uma String. Mas ela precisa de um nome. 
+// Sua tarefa é preencher os espaços com a declaração de variável e dar à ela o nome de exemplo.
+
+const exemplo = function () {
+    return 'Oi, eu sou uma função expressa';
+}
+
+// ##########################################
+
+// Exercício II
+// Agora pense nesse exemplo, temos que validar algumas pessoas pelas suas idades. 
+// Nosso critério será 18 anos como idade miníma e 99 como idade máxima. Ninguém pode mudar essas definições. 
+// Para isso acontecer, iremos escrever constantes. Vale dizer que, por questões de boas práticas,  
+// é comum utilizar nome com letras maiúsculas na hora de definir uma constante.﻿﻿
+
+// Bom agora, você deve criar um constante chamada IDADE_MINIMA, e outra chamada IDADE_MAXIMA. 
+// Que por sua vez, devem ter os valores 18 e 99 respectivamente.
+
+const IDADE_MINIMA = 18
+const IDADE_MAXIMA = 99
+
+// ##########################################
+
