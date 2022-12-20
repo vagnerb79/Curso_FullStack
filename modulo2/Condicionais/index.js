a7aa90e41a0195bb488285a8d1e02c785a1498c6
vagnerb79 <spotify.vagner@gmail.com>
2022-12-20 10:26:27 -0300

    Condicionais

---
+++ modulo2/Condicionais/index.js
@@ -0,0 +1,33 @@
+
+// Exercício I
+//Com este exercício, vamos conseguir fazer um simples "if"."
+    
+//Declare uma variável com o nome dado e atribua um valor booleano.
+//Então você deve fazer um  if para avaliar essa variável: se for true imprimir "É verdadeiro!", caso contrário "É falso!".
+
+let dado = true;
+
+if(dado == true){
+    console.log("É verdadeiro!")
+}else{
+    console.log("É falso!")
+}
+
+// ##########################################
+
+// Exercício II
+//Vamos trabalhar com condicionais agora, fazendo comparações rigorosas/estritas.
+
+//Neste exercício te damos uma variável chamada linguagem, que tem uma String atribuída. 
+//Seu trabalho agora é construir uma condicional if / else para que, se a variável contém "javascript" 
+//imprima no console o texto "Estou aprendendo", se não imprima"Vou aprender mais pra frente".
+
+let linguagem = "javascript"
+if (linguagem === "javascript"){
+    console.log("Estou aprendendo")
+}else{
+    console.log("Vou aprender mais pra frente")
+}
+
+// ##########################################
+
---
+++ modulo2/TiposDados/index.js
@@ -0,0 +1,11 @@
+
+// Exercício I
+//Para este exercício, solicitamos que você declare 4 variáveis ​​usando o let com os nomes: verdadeiro, nada, texto e numero. 
+//Para cada um deles, você deve atribuir o tipo de dados que sugere seu nome, conforme apropriado.
+
+let verdaeiro = true;
+let nada = null;
+let texto = 'Vagner';
+let numero = 13;
+
+// ##########################################
